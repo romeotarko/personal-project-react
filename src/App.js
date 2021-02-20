6fc022e118471cbd5751a5b506b9e6d87d56d1d4
romeotarko <romeo.tarko@outlook.com>
2021-02-20 23:59:05 +0100

    Added:Navbar desktop & mobile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
-import "tailwindcss/tailwind.css"
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello World</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={'Home'} />
+          <Route path='/services' component={'Services'} />
+          <Route path='/products' component={'Product'} />
+          <Route path='/sign-up' component={'Sing up'} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
